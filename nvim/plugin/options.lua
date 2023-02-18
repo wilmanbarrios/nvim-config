de2191ed47f8bf6ea3efe49d7b389ca80314ab20
Wilman Barrios <wilmanbarrios@gmail.com>
2023-02-17 22:16:50 -0400

    Tweak options

--- nvim/plugin/options.lua
+++ nvim/plugin/options.lua
@@ -1,55 +1,6 @@
 local opt = vim.opt
 local g = vim.g
 
--- options
-opt.autoindent            = true
-opt.smartindent           = true
-opt.expandtab             = true
-opt.tabstop               = 2
-opt.shiftwidth            = 2
-opt.softtabstop           = 2
-
-opt.cursorline            = true
-opt.wrap                  = false
-
-opt.hidden                = true
-
-opt.ignorecase            = true
-
-opt.inccommand            = 'split'
-
-opt.colorcolumn           = '+1'
-
-opt.completeopt           = {'menuone', 'noinsert', 'noselect'}
-
-opt.mouse                 = 'a'
-
-opt.backup                = false
-opt.swapfile              = false
-opt.writebackup           = false
-
--- opt.lazyredraw            = true
-
-opt.number                = true
-opt.relativenumber        = true
-opt.signcolumn            = 'yes'
-
-opt.pumblend              = 15
-opt.pumheight             = 20
-
-opt.scrolloff             = 10
-
-opt.smartcase             = true
-
-opt.splitbelow            = true
-opt.splitright            = true
-
-opt.termguicolors         = true
-
-opt.updatetime            = 1000
-
-opt.shortmess:append("c")
-
 -- disable unused builtin plugins
 g.loaded_netrw            = 1
 g.loaded_netrwPlugin      = 1
@@ -65,3 +16,46 @@ g.loaded_perl_provider    = 0
 g.loaded_python_provider  = 0
 g.loaded_python3_provider = 0
 g.loaded_node_provider    = 0
+
+-- options
+opt.autoindent            = true
+opt.smartindent           = true
+opt.expandtab             = true
+opt.tabstop               = 2
+opt.shiftwidth            = 2
+opt.softtabstop           = 2
+
+opt.cursorline            = true
+opt.wrap                  = false
+
+opt.hidden                = true
+
+opt.inccommand            = 'split'
+
+opt.colorcolumn           = '+1'
+
+opt.completeopt           = {'menuone', 'noinsert', 'noselect'}
+
+opt.mouse                 = 'a'
+
+opt.backup                = false
+opt.swapfile              = false
+
+opt.number                = true
+opt.relativenumber        = true
+opt.signcolumn            = 'yes'
+
+opt.pumheight             = 20
+
+opt.scrolloff             = 10
+
+opt.smartcase             = true
+
+opt.splitbelow            = true
+opt.splitright            = true
+
+opt.termguicolors         = true
+
+opt.updatetime            = 50
+
+-- opt.shortmess:append("c") --- will test what is this affecting
