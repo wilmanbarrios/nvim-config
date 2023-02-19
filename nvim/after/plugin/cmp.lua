f93a966a356b7b99b15a93d5e42d644d21cf7c9b
Wilman Barrios <wilmanbarrios@gmail.com>
2023-02-18 23:30:42 -0400

    Remove unneeded comment

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -24,7 +24,6 @@ cmp.setup {
   window = {
     completion = cmp.config.window.bordered(),
     documentation = cmp.config.window.bordered(),
-    -- border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
   },
 
   completion = {
