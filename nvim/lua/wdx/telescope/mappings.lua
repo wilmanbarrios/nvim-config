f419968be35d7aeb0118625a711fc1493ec31d36
Wilman Barrios <wilmanbarrios@gmail.com>
2023-02-18 22:31:37 -0400

    Move telescope mapping function into separate file

--- nvim/lua/wdx/telescope/mappings.lua
+++ nvim/lua/wdx/telescope/mappings.lua
@@ -1,28 +1,4 @@
-if not pcall(require, "telescope") then
-  return
-end
-
-TelescopeMapArgs = TelescopeMapArgs or {}
-
-local map_tele = function(key, f, options, buffer)
-  local map_key = vim.api.nvim_replace_termcodes(key .. f, true, true, true)
-
-  TelescopeMapArgs[map_key] = options or {}
-
-  local mode = "n"
-  local rhs = string.format("<cmd>lua R('wdx.telescope')['%s'](TelescopeMapArgs['%s'])<CR>", f, map_key)
-
-  local map_options = {
-    noremap = true,
-    silent = true,
-  }
-
-  if not buffer then
-    vim.api.nvim_set_keymap(mode, key, rhs, map_options)
-  else
-    vim.api.nvim_buf_set_keymap(0, mode, key, rhs, map_options)
-  end
-end
+local map_tele = require('wdx.telescope.utils')
 
 map_tele("<Leader>p",  "find_files")
 map_tele("<Leader>b",  "buffers")
@@ -36,5 +12,3 @@ map_tele("<Leader>sq", "grep_string")
 -- map_tele("<Leader>fr", "laravel_routes") -- TODO: in progress
 map_tele("<Leader>gr", "lsp_document_symbols")
 vim.keymap.set('n', "<Leader>tt", require('telescope.builtin').resume, map_options)
-
-return map_tele
