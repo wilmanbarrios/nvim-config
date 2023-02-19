1ac8958b7fbc075f05afa04b76cbe37436ad8f94
Wilman Barrios <wilmanbarrios@gmail.com>
2023-02-18 22:35:29 -0400

    Update diagnostic setup

--- nvim/lua/wdx/lsp/diagnostic.lua
+++ nvim/lua/wdx/lsp/diagnostic.lua
@@ -1,11 +1,12 @@
 local signs = { Error = " ", Warn = " ", Hint = "", Info = " " }
+
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
 vim.diagnostic.config({
-  severity_sort = false,
+  severity_sort = true,
   signs = true,
   underline = false,
   update_in_insert = false,
