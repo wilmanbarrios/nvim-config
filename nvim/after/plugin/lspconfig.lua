908567deed88e43a34ad36fe1422c0886dba7652
Wilman Barrios <wilmanbarrios@gmail.com>
2023-02-20 23:57:05 -0400

    Organize LSP config

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -5,29 +5,33 @@ require "wdx.lsp.diagnostic"
 local keymap = vim.keymap.set
 
 local on_attach = function(client, bufnr)
-  -- Diagnostics mappings
   local bufopts = {noremap = true, silent = true, buffer = bufnr}
+
+  -- Diagnostics mappings
   -- keymap("n", "dp", vim.diagnostic.goto_prev, bufopts)
-  keymap("n", "dp", '<cmd>Lspsaga diagnostic_jump_prev<CR>', bufopts)
   -- keymap("n", "dn", vim.diagnostic.goto_next, bufopts)
-  keymap("n", "dn", '<cmd>Lspsaga diagnostic_jump_next<CR>', bufopts)
   -- keymap("n", "dl", vim.diagnostic.open_float, bufopts)
-  keymap("n", "dl", '<cmd>Lspsaga show_line_diagnostics<CR>', bufopts)
   -- keymap("n", "ds", vim.diagnostic.show, bufopts)
   -- keymap("n", "dh", vim.diagnostic.hide, bufopts)
 
   -- LSP mappings
   keymap("n", "gd", vim.lsp.buf.definition, bufopts)
   -- keymap("n", "K", vim.lsp.buf.hover, bufopts)
-  keymap("n", "K", '<cmd>Lspsaga hover_doc<CR>', bufopts)
   keymap("n", "gD", vim.lsp.buf.implementation, bufopts)
   keymap("i", "<c-k>", vim.lsp.buf.signature_help, bufopts)
   keymap("n", "<Leader>D", vim.lsp.buf.type_definition, bufopts)
   -- keymap("n", "<Leader>ca", vim.lsp.buf.code_action, bufopts)
-  keymap("n", "<Leader>ca", '<cmd>Lspsaga code_action<CR>', bufopts)
   keymap("n", "<Leader>ff", function()
     vim.lsp.buf.format({ async = false, bufnr = bufnr })
   end, bufopts)
+
+  --- LSP Saga
+  keymap("n", "dp", '<cmd>Lspsaga diagnostic_jump_prev<CR>', bufopts)
+  keymap("n", "dn", '<cmd>Lspsaga diagnostic_jump_next<CR>', bufopts)
+  keymap("n", "dl", '<cmd>Lspsaga show_line_diagnostics<CR>', bufopts)
+
+  keymap("n", "K", '<cmd>Lspsaga hover_doc<CR>', bufopts)
+  keymap("n", "<Leader>ca", '<cmd>Lspsaga code_action<CR>', bufopts)
 
   -- disable `tsserver` formatting in favor of null-ls `prettierd` formatting
   if client.name == "tsserver" then
@@ -36,14 +40,8 @@ local on_attach = function(client, bufnr)
   end
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
 local servers = {
   tailwindcss = {},
-  html = {},
-  cssls = {},
-  prismals = {},
   intelephense = {
     init_options = {
       globalStoragePath = os.getenv("XDG_DATA_HOME") .. "/intelephense",
@@ -69,8 +67,10 @@ local servers = {
       }
     }
   },
-  vuels = {},
 }
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 for server, opts in pairs(servers) do
   lspconfig[server].setup(vim.tbl_extend("force", {
