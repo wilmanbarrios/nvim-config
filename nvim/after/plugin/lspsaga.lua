22a6cb95b6810200fd681d0a826f4feefc3c3a43
Wilman Barrios <wilmanbarrios@gmail.com>
2023-02-20 23:57:52 -0400

    I'm trying `lsp-saga`

--- nvim/after/plugin/lspsaga.lua
+++ nvim/after/plugin/lspsaga.lua
@@ -1,6 +1,16 @@
 require("lspsaga").setup({
   ui = {
-    -- winblend = 16,
+    title = false,
     border = 'rounded',
-  }
+  },
+  lightbulb = {
+    enable = true,
+    enable_in_insert = true,
+    sign = true,
+    sign_priority = 40,
+    virtual_text = false,
+  },
+  symbol_in_winbar = {
+    enable = false
+  },
 })
