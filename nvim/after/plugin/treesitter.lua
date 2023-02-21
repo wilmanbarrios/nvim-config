ef322c22e204e64da7c1b013801dc4160573b8be
Wilman Barrios <wilmanbarrios@gmail.com>
2023-02-21 00:29:04 -0400

    Only use needed TS parser, improve startuptime

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,39 +1,42 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = "all",
+  ensure_installed = { "javascript", "comment", 'css', 'diff', 'gitattributes',
+  'gitcommit', 'graphql', 'help', 'html', 'jsdoc', 'json', 'lua', 'markdown',
+  'markdown_inline', 'php', 'phpdoc', 'prisma', 'regex', 'sql', 'tsx',
+  'typescript', 'yaml', 'query' },
   highlight = {
     enable = true,
   },
-  indent = {
-    enable = true
-  },
-  context_commentstring = {
-    enable = true
-  },
-  refactor = {
-    highlight_definitions = {
-      enable = true
-    },
-    smart_rename = {
-      enable = true,
-    },
-  },
-  autotag = {
-    enable = true,
-  },
-  context = {
-    enable = true,
-  },
-  textobjects = {
-    swap = {
-      enable = true,
-      swap_next = {
-        ["L"] = "@parameter.inner",
-      },
-      swap_previous = {
-        ["H"] = "@parameter.inner",
-      },
-    },
-  },
+  -- indent = {
+  --   enable = true
+  -- },
+  -- context_commentstring = {
+  --   enable = true
+  -- },
+  -- refactor = {
+  --   highlight_definitions = {
+  --     enable = true
+  --   },
+  --   smart_rename = {
+  --     enable = true,
+  --   },
+  -- },
+  -- autotag = {
+  --   enable = true,
+  -- },
+  -- context = {
+  --   enable = true,
+  -- },
+  -- textobjects = {
+  --   swap = {
+  --     enable = true,
+  --     swap_next = {
+  --       ["L"] = "@parameter.inner",
+  --     },
+  --     swap_previous = {
+  --       ["H"] = "@parameter.inner",
+  --     },
+  --   },
+  -- },
 }
 
 vim.cmd[[
