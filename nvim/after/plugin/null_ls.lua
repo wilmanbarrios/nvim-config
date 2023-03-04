8071a6a67c22107dccb8cc63e61499e8b1e6feaf
Wilman Barrios <wilmanbarrios@gmail.com>
2023-03-03 22:42:12 -0400

    WIP: disable eslint_d for now, noticing and annoying slowness when moving cursor

--- nvim/after/plugin/null_ls.lua
+++ nvim/after/plugin/null_ls.lua
@@ -3,10 +3,5 @@ local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettierd,
-
-    -- Eslint
-    null_ls.builtins.diagnostics.eslint_d,
-    null_ls.builtins.code_actions.eslint_d,
-    -- null_ls.builtins.formatting.eslint_d,
   },
 })
