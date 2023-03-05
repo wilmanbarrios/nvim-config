889871b320be7677c15746c69c4d8f86eee5f6c6
Wilman Barrios <wilmanbarrios@gmail.com>
2023-03-05 00:32:27 -0400

    Migrate plugin config to lua

--- nvim/lua/wdx/plugins/test.lua
+++ nvim/lua/wdx/plugins/test.lua
@@ -4,6 +4,13 @@ return {
   dependencies = {
     'benmills/vimux',
   },
+  config = function()
+    vim.g.VimuxHeight = '40'
+    vim.g.VimuxOrientation = 'h'
+    vim.g['test#neovim#term_position'] = "vert botright 98"
+    vim.g['test#strategy'] = "vimux"
+    vim.g['test#php#dusk#executable'] = "php artisan dusk --pest"
+  end,
   keys = {
     { '<Leader>tr', '<cmd>TestNearest<CR>', desc = 'Run nearest test' },
     { '<Leader>tf', '<cmd>TestFile<CR>', desc = 'Run current test file' },
