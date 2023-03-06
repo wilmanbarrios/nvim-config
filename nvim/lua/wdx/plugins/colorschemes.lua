7b29b79ba620ae0f5f43b05c0dd0b074e8d9cfa6
Wilman Barrios <wilmanbarrios@gmail.com>
2023-03-06 06:48:11 -0400

    Fix tokyonight not using my colors custom colors

--- nvim/lua/wdx/plugins/colorschemes.lua
+++ nvim/lua/wdx/plugins/colorschemes.lua
@@ -2,9 +2,6 @@ return {
    {
      'folke/tokyonight.nvim',
      priority = 1000,
-     config = function()
-       vim.cmd.colorscheme('tokyonight-night')
-     end,
      opts = function()
        local util = require("tokyonight.util")
        return {
@@ -13,9 +10,14 @@ return {
            hl["@text.todo"] = { fg = c.yellow, bg = util.darken(c.yellow, 0.2) }
            hl["@text.danger"] = { fg = c.error, bg = util.darken(c.error, 0.2) }
            -- comment
-           hl["@comment"] = { fg = util.lighten(c.comment, 0.5) }
+           hl["@comment"] = { fg = util.lighten(c.comment, 0.8) }
          end,
        }
+     end,
+     config = function(_, opts)
+        require('tokyonight').setup(opts)
+
+       vim.cmd.colorscheme('tokyonight-night')
      end,
    },
    {
