044421e7cda3a668385f5b302beb31e01f590a81
Wilman Barrios <wilmanbarrios@gmail.com>
2023-03-10 23:12:39 -0400

    Tweak linenumbers colors a bit

--- nvim/lua/wdx/plugins/colorschemes.lua
+++ nvim/lua/wdx/plugins/colorschemes.lua
@@ -9,15 +9,14 @@ return {
          on_highlights = function(hl, c)
            hl["@text.todo"] = { fg = c.yellow, bg = util.darken(c.yellow, 0.2) }
            hl["@text.danger"] = { fg = c.error, bg = util.darken(c.error, 0.2) }
-           -- comment
            hl["@comment"] = { fg = util.lighten(c.comment, 0.8) }
+           hl.LineNr = { fg = util.lighten(c.comment, 1) }
          end,
        }
      end,
      config = function(_, opts)
-        require('tokyonight').setup(opts)
-
-       vim.cmd.colorscheme('tokyonight-night')
+       require('tokyonight').setup(opts)
+       vim.cmd.colorscheme('tokyonight')
      end,
    },
    {
