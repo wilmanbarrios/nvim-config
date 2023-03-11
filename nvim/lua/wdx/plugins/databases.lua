f99d36c8335792875b9fcd78d786c309a33b3aa9
Wilman Barrios <wilmanbarrios@gmail.com>
2023-03-10 23:09:12 -0400

    Fix query run mapping

--- nvim/lua/wdx/plugins/databases.lua
+++ nvim/lua/wdx/plugins/databases.lua
@@ -2,7 +2,7 @@ return {
   'tpope/vim-dadbod',
   cmd = 'DB',
   keys = {
-    { mode = 'x', '<Leader>rq', '<cmd>DB<CR> <Bar> <cmd>up<CR>', desc = "Visual select any DB queries and run them on the fly" }
+    { mode = 'x', '<Leader>rq', ':DB<CR> | :up<CR>', desc = "Visual select any DB queries and run them on the fly" }
   },
   dependencies = {
     'tpope/vim-dotenv',
