89fca11506ae681e673a249f61fd62142a937c83
Wilman Barrios <wilmanbarrios@gmail.com>
2023-03-19 19:43:18 -0400

    Exclude `gitcommit` from fix for folds

--- nvim/plugin/autocmds.lua
+++ nvim/plugin/autocmds.lua
@@ -1,5 +1,8 @@
 vim.api.nvim_create_autocmd('BufRead', {
   callback = function()
+    if vim.bo.filetype == 'gitcommit' then
+      return
+    end
     vim.api.nvim_create_autocmd('BufWinEnter', {
       once = true,
       command = 'normal! zx | zR'
