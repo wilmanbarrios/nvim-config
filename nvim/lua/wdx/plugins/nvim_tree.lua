0628aeeb940597c33d1cc7868fefb3e20c283ac3
Wilman Barrios <wilmanbarrios@gmail.com>
2023-08-14 10:22:36 -0400

    Move nvim-tree `view.mappings` to `on_attach`

--- nvim/lua/wdx/plugins/nvim_tree.lua
+++ nvim/lua/wdx/plugins/nvim_tree.lua
@@ -1,3 +1,28 @@
+-- TODO(wilman): describe
+local register_listeners = function()
+  local api = require('nvim-tree.api')
+  local Event = api.events.Event
+
+  local update_symlinks = function()
+    local has_dodbot = vim.fn.getcwd():match('nvim%-config')
+
+    if not has_dodbot then
+      return
+    end
+
+    vim.schedule(function()
+      vim.fn.system({ './install', '-Q' })
+      vim.notify('symlinks installed!')
+    end)
+  end
+
+  api.events.subscribe(Event.NodeRenamed, update_symlinks)
+  api.events.subscribe(Event.FolderCreated, update_symlinks)
+  api.events.subscribe(Event.FolderRemoved, update_symlinks)
+  api.events.subscribe(Event.FileCreated, update_symlinks)
+  api.events.subscribe(Event.FileRemoved, update_symlinks)
+end
+
 return {
   {
     'nvim-tree/nvim-tree.lua',
@@ -15,49 +40,39 @@ return {
       view = {
         width = 41,
         side = "right",
-        mappings = {
-          custom_only = false,
-          list = {
-            { key = "h", action = "close_node" },
-            { key = "l", action = "edit" },
-          }
-        }
       },
     },
     config = function(_, opts)
-      require('nvim-tree').setup(opts)
+      require('nvim-tree').setup(vim.tbl_extend("force", {
+        on_attach = function(bufnr)
+          local api = require('nvim-tree.api')
 
-      local api = require('nvim-tree.api')
-      local Event = api.events.Event
+          local function opts(desc)
+            return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+          end
 
-      local update_symlinks = function()
-        local has_dodbot = vim.fn.getcwd():match('nvim%-config') 
+          api.config.mappings.default_on_attach(bufnr)
 
-        if not has_dodbot then
-          return
-        end
-
-        vim.schedule(function()
-          vim.fn.system({ './install', '-Q' })
-          vim.notify('symlinks installed!')
-        end)
-      end
-
-      api.events.subscribe(Event.NodeRenamed, update_symlinks)
-      api.events.subscribe(Event.FolderCreated, update_symlinks)
-      api.events.subscribe(Event.FolderRemoved, update_symlinks)
-      api.events.subscribe(Event.FileCreated, update_symlinks)
-      api.events.subscribe(Event.FileRemoved, update_symlinks)
-    end,
-    keys = {
-      {"<C-b>", '<cmd>NvimTreeToggle<cr>', desc = 'Open/close nvim-tree'},
-      {
-        "<Leader>l",
-        function()
-          require'nvim-tree.api'.tree.open({ find_file = true })
+          vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close node'))
+          vim.keymap.set('n', 'l', api.node.open.edit, opts('Open node'))
         end,
-        desc = 'Open the current buffer on the tree, or focusing it if already open.'
-      },
-    }
+      }, opts))
+
+      register_listeners()
+    end,
+    keys = function()
+      local api = require'nvim-tree.api'
+
+      return {
+        {"<C-b>", api.tree.toggle, desc = 'Open/close nvim-tree'},
+        {
+          "<Leader>l",
+          function()
+            api.tree.find_file({ open = true })
+          end,
+          desc = 'Open the current buffer on the tree, or focusing it if already open.'
+        },
+      }
+    end,
   },
 }
