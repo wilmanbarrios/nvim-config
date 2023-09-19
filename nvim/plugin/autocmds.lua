db006c5e64831a60fa0539c626b90137b8cc6927
Wilman Barrios <wilmanbarrios@gmail.com>
2023-09-18 21:16:58 -0400

    Only autocomplete COOP issue code if current branch is not `develo`

--- nvim/plugin/autocmds.lua
+++ nvim/plugin/autocmds.lua
@@ -15,11 +15,22 @@ vim.api.nvim_create_autocmd("BufRead", {
   end,
 })
 
---- If we are creating a commit on a COOP repo auto type for me the COOPFE issue code
+local function branch_name()
+  local branch =
+    vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
+  if branch ~= "" then
+    return branch
+  else
+    return ""
+  end
+end
+
+--- If we are creating a commit on a COOP repo and current branch is not
+--- `develop` auto type for me the COOPFE issue code
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "gitcommit",
   callback = function(ev)
-    if string.find(ev.file, "COOP") then
+    if string.find(ev.file, "COOP") and branch_name() ~= "develop" then
       vim.api.nvim_feedkeys("4jf/wveeeyggPa: ", "n", true)
     end
   end,
