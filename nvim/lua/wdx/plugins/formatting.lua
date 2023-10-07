5f198c2b2e26be569ed1ab5da3c23c4388afe19f
Wilman Barrios <wilmanbarrios@gmail.com>
2023-10-07 12:34:25 -0400

    Testing biome

--- nvim/lua/wdx/plugins/formatting.lua
+++ nvim/lua/wdx/plugins/formatting.lua
@@ -1,18 +1,20 @@
+local javascript_formatter = { { "biome", "prettierd", "prettier" } }
+
 return {
   {
     "stevearc/conform.nvim",
     main = "conform",
     opts = {
+      log_level = vim.log.levels.DEBUG,
       format_on_save = {
-        -- These options will be passed to conform.format()
         timeout_ms = 500,
         lsp_fallback = true,
       },
       formatters_by_ft = {
         lua = { "stylua" },
-        javascript = { { "prettierd", "prettier" }, "eslint_d" },
-        typescript = { { "prettierd", "prettier" }, "eslint_d" },
-        typescriptreact = { { "prettierd", "prettier" }, "eslint_d" },
+        javascript = javascript_formatter,
+        typescript = javascript_formatter,
+        typescriptreact = javascript_formatter,
       },
     },
   },
