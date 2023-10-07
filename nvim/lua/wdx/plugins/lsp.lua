b7a3ccf8cc497f27a70e3dace9ee8cecb304e183
Wilman Barrios <wilmanbarrios@gmail.com>
2023-10-07 12:33:46 -0400

    Small improvement to nvim-lint

--- nvim/lua/wdx/plugins/lsp.lua
+++ nvim/lua/wdx/plugins/lsp.lua
@@ -45,17 +45,21 @@ return {
   {
     "mfussenegger/nvim-lint",
     config = function()
-      require("lint").linters_by_ft = {
+      local lint = require("lint")
+      lint.linters_by_ft = {
         javascript = { "eslint_d" },
         typescript = { "eslint_d" },
         typescriptreact = { "eslint_d" },
       }
 
-      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-        callback = function()
-          require("lint").try_lint()
-        end,
-      })
+      vim.api.nvim_create_autocmd(
+        { "BufEnter", "BufWritePost", "InsertLeave" },
+        {
+          callback = function()
+            lint.try_lint()
+          end,
+        }
+      )
     end,
   },
 }
