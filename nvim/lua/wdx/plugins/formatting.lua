6805d5e14c7f1a2694053301dafb9faa56646cf7
Wilman Barrios <wilmanbarrios@gmail.com>
2023-11-07 21:51:25 -0400

    Testing formatting configuration

--- nvim/lua/wdx/plugins/formatting.lua
+++ nvim/lua/wdx/plugins/formatting.lua
@@ -4,6 +4,8 @@ return {
   {
     "stevearc/conform.nvim",
     main = "conform",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
     opts = {
       log_level = vim.log.levels.DEBUG,
       format_on_save = {
@@ -15,6 +17,24 @@ return {
         javascript = javascript_formatter,
         typescript = javascript_formatter,
         typescriptreact = javascript_formatter,
+      },
+    },
+    init = function()
+      -- TODO: just testing this thing because the docs says: "Expression which
+      -- is evaluated to format a range of lines for the |gq| operator or
+      -- automatic formatting"
+
+      -- If you want the formatexpr, here is the place to set it
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+    keys = {
+      {
+        mode = { "n", "v" },
+        "<leader>ff",
+        function()
+          require("conform").format({ async = true, lsp_fallback = true })
+        end,
+        desc = "Format buffer",
       },
     },
   },
