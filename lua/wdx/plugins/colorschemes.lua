b7ad05838e01cc2d8c3b5ef87381cd41d929a07d
Wilman Barrios <wilmanbarrios@gmail.com>
2025-02-21 19:50:45 -0400

    Dark+ default colorscheme

--- lua/wdx/plugins/colorschemes.lua
+++ lua/wdx/plugins/colorschemes.lua
@@ -18,7 +18,7 @@ return {
     end,
     config = function(_, opts)
       require("tokyonight").setup(opts)
-      vim.cmd.colorscheme("tokyonight")
+      -- vim.cmd.colorscheme("tokyonight")
     end,
   },
   {
@@ -33,6 +33,76 @@ return {
     config = function(_, opts)
       require("rose-pine").setup(opts)
       -- vim.cmd.colorscheme("rose-pine")
+    end,
+  },
+  {
+    "Mofiqul/vscode.nvim",
+    enable = false,
+    priority = 1000,
+    opts = function()
+      -- local c = require("vscode.colors").get_colors()
+
+      return {
+        italic_comments = true,
+        disable_nvimtree_bg = true,
+        -- group_overrides = {
+        --   ["@variable"] = { fg = c.vscBlue, bg = c.vscNone },
+        --   ["@variable.builtin"] = { fg = c.vscBlue, bg = c.vscNone },
+        --
+        --   ["@lsp.typemod.variable.defaultLibrary"] = { link = "@type" },
+        --   ["@lsp.typemod.type.defaultLibrary"] = { link = "@type" },
+        --   ["@lsp.typemod.variable"] = { link = "@constant" },
+        --   -- ['@lsp.typemod.variable.declaration'] = { link = '@variable' },
+        --
+        --   ["@type.builtin"] = { link = "@type" },
+        --   ["@tag.tsx"] = { link = "@type" },
+        --
+        --   SpecialChar = { link = "Special" },
+        --   ["@string.regexp"] = { fg = c.vscLightRed, bg = c.vscNone },
+        --   ["@character.special"] = { fg = c.vscBlue, bg = c.vscNone },
+        --
+        --   -- spell
+        --   SpellBad = {
+        --     fg = c.vscNone,
+        --     bg = c.vscNone,
+        --     undercurl = true,
+        --     sp = c.vscRed,
+        --   },
+        --   SpellCap = {
+        --     fg = c.vscNone,
+        --     bg = c.vscNone,
+        --     undercurl = true,
+        --     sp = c.vscRed,
+        --   },
+        --   SpellRare = {
+        --     fg = c.vscNone,
+        --     bg = c.vscNone,
+        --     undercurl = true,
+        --     sp = c.vscRed,
+        --   },
+        --   SpellLocal = {
+        --     fg = c.vscNone,
+        --     bg = c.vscNone,
+        --     undercurl = true,
+        --     sp = c.vscRed,
+        --   },
+        --
+        --   ModeMsg = { fg = c.vscFront, bg = c.vscNone },
+        --   WarningMsg = { fg = c.vscYellowOrange, bg = c.vscNone },
+        --
+        --   GitBranch = { fg = c.vscGreen, bg = c.vscNone },
+        --
+        --   NonText = {
+        --     fg = c.vscLineNumber,
+        --     bg = c.vscNone,
+        --   },
+        -- },
+      }
+    end,
+    config = function(_, opts)
+      local vscode = require("vscode")
+      vscode.setup(opts)
+      vscode.load()
     end,
   },
 }
