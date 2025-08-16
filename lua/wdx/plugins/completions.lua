1c897a17797f01bb41656006852557be78bc2ba2
Wilman Barrios <wilmanbarrios@gmail.com>
2025-08-16 19:53:24 -0400

    Remove lazy.dev config from completion

--- lua/wdx/plugins/completions.lua
+++ lua/wdx/plugins/completions.lua
@@ -15,14 +15,7 @@ return {
       },
       completion = { documentation = { auto_show = true } },
       sources = {
-        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-        providers = {
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            score_offset = 100,
-          },
-        },
+        default = { "lsp", "path", "snippets", "buffer" },
       },
       fuzzy = {
         implementation = "prefer_rust_with_warning",
