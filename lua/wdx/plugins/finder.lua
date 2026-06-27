e21c55ffb1bf2d17ca767ad64f44a829cb371221
Wilman Barrios <wilmanbarrios@gmail.com>
2026-06-27 12:49:09 -0400

    Replace telescope with fzf-lua finder
    - Comment out telescope; switch finder keymaps to fzf-lua
    - Map <Leader>p/b/fd/ht/sw/sq/grr/tt to fzf-lua equivalents
    - Preserve dotfiles git_files (recurse-submodules, exclude dotbot)
    - Hide cwd from prompt and results header

--- lua/wdx/plugins/finder.lua
+++ lua/wdx/plugins/finder.lua
@@ -1,82 +1,225 @@
+local function is_dotfiles()
+  local cwd = vim.fn.getcwd()
+  local dirs = vim.split(cwd, "/")
+  return dirs[#dirs] == "dotfiles"
+end
+
+local function dotfiles_files()
+  require("fzf-lua").git_files({
+    cmd = "git -c core.quotepath=false ls-files --exclude-standard "
+      .. "--cached --recurse-submodules :!:dotbot",
+    cwd = vim.fn.expand("$DOTFILES"),
+    prompt = "❯ ",
+  })
+end
+
 return {
+  -- {
+  --   "nvim-telescope/telescope.nvim",
+  --   branch = "master",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-telescope/telescope-ui-select.nvim",
+  --     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  --   },
+  --   opts = function()
+  --     local actions = require("telescope.actions")
+  --     local themes = require("telescope.themes")
+  --
+  --     return {
+  --       defaults = {
+  --         prompt_prefix = " ",
+  --         selection_caret = " ",
+  --         dynamic_preview_title = true,
+  --         mappings = {
+  --           i = {
+  --             ["<esc>"] = actions.close,
+  --             ["<c-x>"] = false, -- default open file in split
+  --             ["<c-s>"] = actions.select_horizontal, -- open file in split
+  --             ["<c-q>"] = actions.send_to_qflist + actions.open_qflist,
+  --           },
+  --         },
+  --       },
+  --       extensions = {
+  --         fzf = {
+  --           fuzzy = true, -- false will only do exact matching
+  --           override_generic_sorter = true, -- override the generic sorter
+  --           override_file_sorter = true, -- override the file sorter
+  --         },
+  --         ["ui-select"] = {
+  --           themes.get_ivy({
+  --             layout_config = {
+  --               height = 10,
+  --             },
+  --           }),
+  --         },
+  --       },
+  --     }
+  --   end,
+  --   cmd = "Telescope",
+  --   keys = {
+  --     {
+  --       "<Leader>p",
+  --       function()
+  --         local utils = require("telescope.utils")
+  --         local git_command = utils.__git_command
+  --         local cwd = vim.fn.getcwd()
+  --         local dirs = vim.split(cwd, "/")
+  --         local last_dir = dirs[#dirs]
+  --         local is_dotfiles = last_dir == "dotfiles"
+  --
+  --         if is_dotfiles then
+  --           require("telescope.builtin").git_files({
+  --             git_command = git_command({
+  --               "-c",
+  --               "core.quotepath=false",
+  --               "ls-files",
+  --               "--exclude-standard",
+  --               "--cached",
+  --               "--recurse-submodules",
+  --               ":!:dotbot",
+  --             }),
+  --             cwd = "$DOTFILES",
+  --             prompt_title = "Dotfiles",
+  --           })
+  --         else
+  --           require("telescope.builtin").find_files({
+  --             find_command = {
+  --               "fd",
+  --               "--type",
+  --               "f",
+  --               "--strip-cwd-prefix",
+  --               "-E",
+  --               "dotbot",
+  --             },
+  --           })
+  --         end
+  --       end,
+  --     },
+  --     {
+  --       "<Leader>b",
+  --       function()
+  --         require("telescope.builtin").buffers({
+  --           ignore_current_buffer = true,
+  --           previewer = false,
+  --           sort_mru = true,
+  --           sorting_strategy = "ascending",
+  --           layout_config = {
+  --             prompt_position = "top",
+  --             width = 100,
+  --             height = 20,
+  --           },
+  --         })
+  --       end,
+  --     },
+  --     {
+  --       "<Leader>fd",
+  --       function()
+  --         local utils = require("telescope.utils")
+  --         local git_command = utils.__git_command
+  --
+  --         require("telescope.builtin").git_files({
+  --           git_command = git_command({
+  --             "-c",
+  --             "core.quotepath=false",
+  --             "ls-files",
+  --             "--exclude-standard",
+  --             "--cached",
+  --             "--recurse-submodules",
+  --             ":!:dotbot",
+  --           }),
+  --           cwd = "$DOTFILES",
+  --           prompt_title = "Dotfiles",
+  --         })
+  --       end,
+  --     },
+  --     {
+  --       "<Leader>ht",
+  --       function()
+  --         require("telescope.builtin").help_tags()
+  --       end,
+  --     },
+  --     {
+  --       "<Leader>sw",
+  --       function()
+  --         local text =
+  --           vim.fn.input("Grep String 👉 ", vim.fn.expand("<cword>"))
+  --
+  --         if text == "" then
+  --           return
+  --         end
+  --
+  --         require("telescope.builtin").grep_string({
+  --           path_display = { "shorten" },
+  --           search = text,
+  --         })
+  --       end,
+  --     },
+  --     {
+  --       "<Leader>sq",
+  --       function()
+  --         require("telescope.builtin").grep_string()
+  --       end,
+  --     },
+  --     {
+  --       "<Leader>grr",
+  --       function()
+  --         require("telescope.builtin").lsp_document_symbols()
+  --       end,
+  --     },
+  --     {
+  --       "<Leader>tt",
+  --       function()
+  --         require("telescope.builtin").resume()
+  --       end,
+  --     },
+  --   },
+  --   config = function(_, opts)
+  --     require("telescope").setup(opts)
+  --     require("telescope").load_extension("fzf")
+  --     require("telescope").load_extension("ui-select")
+  --   end,
+  -- },
+
   {
-    "nvim-telescope/telescope.nvim",
-    branch = "master",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope-ui-select.nvim",
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    },
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = function()
-      local actions = require("telescope.actions")
-      local themes = require("telescope.themes")
+      local actions = require("fzf-lua").actions
 
       return {
+        -- "ivy",
         defaults = {
-          prompt_prefix = " ",
-          selection_caret = " ",
-          dynamic_preview_title = true,
-          mappings = {
-            i = {
-              ["<esc>"] = actions.close,
-              ["<c-x>"] = false, -- default open file in split
-              ["<c-s>"] = actions.select_horizontal, -- open file in split
-              ["<c-q>"] = actions.send_to_qflist + actions.open_qflist,
-            },
+          cwd_header = false, -- hide cwd from results header
+          cwd_prompt = false, -- hide cwd from prompt
+        },
+        files = {
+          cwd_prompt = false,
+          prompt = "❯ ",
+        },
+        keymap = {
+          fzf = {
+            ["esc"] = "abort",
           },
         },
-        extensions = {
-          fzf = {
-            fuzzy = true, -- false will only do exact matching
-            override_generic_sorter = true, -- override the generic sorter
-            override_file_sorter = true, -- override the file sorter
-          },
-          ["ui-select"] = {
-            themes.get_ivy({
-              layout_config = {
-                height = 10,
-              },
-            }),
+        actions = {
+          files = {
+            ["enter"] = actions.file_edit_or_qf,
+            ["ctrl-s"] = actions.file_split, -- open file in split
+            ["ctrl-q"] = actions.file_sel_to_qf, -- send to qflist
           },
         },
       }
     end,
-    cmd = "Telescope",
     keys = {
       {
         "<Leader>p",
         function()
-          local utils = require("telescope.utils")
-          local git_command = utils.__git_command
-          local cwd = vim.fn.getcwd()
-          local dirs = vim.split(cwd, "/")
-          local last_dir = dirs[#dirs]
-          local is_dotfiles = last_dir == "dotfiles"
-      
-          if is_dotfiles then
-            require("telescope.builtin").git_files({
-              git_command = git_command({
-                "-c",
-                "core.quotepath=false",
-                "ls-files",
-                "--exclude-standard",
-                "--cached",
-                "--recurse-submodules",
-                ":!:dotbot",
-              }),
-              cwd = "$DOTFILES",
-              prompt_title = "Dotfiles",
-            })
+          if is_dotfiles() then
+            dotfiles_files()
           else
-            require("telescope.builtin").find_files({
-              find_command = {
-                "fd",
-                "--type",
-                "f",
-                "--strip-cwd-prefix",
-                "-E",
-                "dotbot",
-              },
+            require("fzf-lua").files({
+              cmd = "fd --type f --strip-cwd-prefix -E dotbot",
             })
           end
         end,
@@ -84,44 +227,25 @@ return {
       {
         "<Leader>b",
         function()
-          require("telescope.builtin").buffers({
+          require("fzf-lua").buffers({
             ignore_current_buffer = true,
+            sort_lastused = true,
             previewer = false,
-            sort_mru = true,
-            sorting_strategy = "ascending",
-            layout_config = {
-              prompt_position = "top",
-              width = 100,
-              height = 20,
+            winopts = {
+              height = 0.4,
+              width = 0.5,
             },
           })
         end,
       },
       {
         "<Leader>fd",
-        function()
-          local utils = require("telescope.utils")
-          local git_command = utils.__git_command
-
-          require("telescope.builtin").git_files({
-            git_command = git_command({
-              "-c",
-              "core.quotepath=false",
-              "ls-files",
-              "--exclude-standard",
-              "--cached",
-              "--recurse-submodules",
-              ":!:dotbot",
-            }),
-            cwd = "$DOTFILES",
-            prompt_title = "Dotfiles",
-          })
-        end,
+        dotfiles_files,
       },
       {
         "<Leader>ht",
         function()
-          require("telescope.builtin").help_tags()
+          require("fzf-lua").helptags()
         end,
       },
       {
@@ -134,63 +258,27 @@ return {
             return
           end
 
-          require("telescope.builtin").grep_string({
-            path_display = { "shorten" },
-            search = text,
-          })
+          require("fzf-lua").grep({ search = text })
         end,
       },
       {
         "<Leader>sq",
         function()
-          require("telescope.builtin").grep_string()
+          require("fzf-lua").grep_cword()
         end,
       },
       {
         "<Leader>grr",
         function()
-          require("telescope.builtin").lsp_document_symbols()
+          require("fzf-lua").lsp_document_symbols()
         end,
       },
       {
         "<Leader>tt",
         function()
-          require("telescope.builtin").resume()
+          require("fzf-lua").resume()
         end,
       },
     },
-    config = function(_, opts)
-      require("telescope").setup(opts)
-      require("telescope").load_extension("fzf")
-      require("telescope").load_extension("ui-select")
-    end,
   },
-
-  -- {
-  --   "ibhagwan/fzf-lua",
-  --   dependencies = { "nvim-tree/nvim-web-devicons" },
-  --   opts = {
-  --     files = {
-  --       cwd_prompt = false,
-  --     },
-  --   },
-  --   keys = {
-  --     {
-  --       "<Leader>p",
-  --       ":lua FzfLua.files()<CR>",
-  --     },
-  --     {
-  --       "<Leader>ht",
-  --       ":lua FzfLua.helptags()<CR>",
-  --     },
-  --     {
-  --       "<Leader>grr",
-  --       ":lua FzfLua.lsp_document_symbols()<CR>",
-  --     },
-  --     {
-  --       "<Leader>tt",
-  --       ":lua FzfLua.resume()<CR>",
-  --     },
-  --   },
-  -- },
 }
